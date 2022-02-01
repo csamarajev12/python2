e051987e592d854ad27dcb2ea9e56ba88fc7b78b
csamarajev12 <98739412+csamarajev12@users.noreply.github.com>
2022-02-01 13:57:35 +0200

    Add files via upload

---
+++ yl2.py
@@ -0,0 +1,61 @@
+import math
+#Harjutus 02
+#Caroline Samarajev
+#01.02.22
+
+#kytusekulu arvutamine
+
+
+
+
+
+
+
+
+
+#9 arvusütemid
+
+
+
+
+
+
+#8 ajateisendus
+minutid = int(input("Sistesta minutid: "))
+tund =  1 #täisarvuline jagamine
+minut=  12 #jäägi leidmine
+print(minutid,"minutit on", tund,":", minut)
+
+
+#7 kolmurga hüpo.
+a,b = 16,9
+c = math.sqrt(a**2+b**2)
+print("hüpotenuus: ",c)
+
+#6 rulluisutajad
+kiirus = 29.9
+minut = kiirus/60
+vastus = minut*24
+print()
+
+
+#5 Pitsa
+hind = 12.90
+jootraha = 0.1
+
+
+
+
+
+
+#4 toote hind
+hind = 36.75
+ale = 0.4
+kogus = 3
+summa = (hind-(hind*soodus))*kogus
+print(kogus, "toote hind",summa,"€")
+
+#3 kolmnurga ümbermõõt
+a,c,b = 15,16,18
+p=a+b+c
+print("Kolmnurga ümbermõõt: ",p)
